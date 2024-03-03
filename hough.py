d7a709ee814298ce242c77a664eb25d36858ea15
Praful Nair <prafulworkspace@gmail.com>
2024-03-02 23:33:24 -0500

    Created a mask for ROI Then muliplied the mask to the canny edge. Wrote a display function to display the results

--- hough.py
+++ hough.py
@@ -9,36 +9,41 @@ from skimage.io import imread
 import matplotlib.pyplot as plt
 from utils import create_line, create_mask
 
+
+def display_Stuff(image, edge1, edges_roi):
+    ## THIS IS MY STEP
+    ## DISPLAY THE RESULTS
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+
+    ax[0].imshow(image, cmap='gray')
+    ax[0].set_title('The IMage', fontsize=8)
+
+    ax[1].imshow(edge1, cmap='gray')
+    ax[1].set_title('Canny Edges sigma=1.5', fontsize=8)
+
+    ax[2].imshow(edges_roi, cmap='gray')
+    ax[2].set_title('Edges ROI', fontsize=8)
+
+    for a in ax:
+        a.axis('off')
+    fig.tight_layout()
+    plt.show()
+
 # load the input image
 image = imread('road.jpg', as_gray=True)
 
 # run Canny edge detector to find edge points
-edge1 = feature.canny(image, sigma = 2)
-edge2 = feature.canny(image, sigma=3)
-edge3 = feature.canny(image, sigma=8)
+edge1 = feature.canny(image)
 
-## THIS IS MY STEP
-## DISPLAY THE RESULTS
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 3))
+#display_Stuff(image, edge1)
 
-ax[0].imshow(image, cmap='gray')
-ax[0].set_title('The IMage', fontsize=8)
-
-ax[1].imshow(edge1, cmap='gray')
-ax[1].set_title('Canny Edges sigma=1.5', fontsize=8)
-#
-# ax[2].imshow(edge2, cmap='gray')
-# ax[2].set_title('Canny Edges sigma=3', fontsize=8)
-#
-# ax[3].imshow(edge3, cmap='gray')
-# ax[3].set_title('Canny Edges sigma=8', fontsize=8)
-for a in ax:
-    a.axis('off')
-fig.tight_layout()
-plt.show()
 # create a mask for ROI by calling create_mask
+mask_roi = create_mask(edge1.shape[0], edge1.shape[1])
 
 # extract edge points in ROI by multipling edge map with the mask
+edges_roi = edge1 * mask_roi
+display_Stuff(image, edge1, edges_roi)
+
 
 # perform Hough transform
 
